866cd26d0e60849c5283287451a2fe4e2ef11a8e
Gabriel <gabriel.r_00@hotmail.com>
2022-12-01 19:18:00 -0300

    primeiro exercicio parte2

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-5-javascript-ES6/excParte2/exercicio.js
@@ -0,0 +1,7 @@
+let statusCar = 'desligado'
+
+const ligarDesligar = () => statusCar == 'desligado' ? statusCar = 'ligado' : statusCar = 'desligado';
+
+console.log(`O motor está: ${ligarDesligar()}`);
+console.log(`O motor está: ${ligarDesligar()}`);
+console.log(`O motor está: ${ligarDesligar()}`);
