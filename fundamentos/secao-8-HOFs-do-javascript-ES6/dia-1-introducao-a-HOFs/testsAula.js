cfbfc70e0fdf24496c3d06b887deae9763cfd1b3
gabri10on <gabriel.r_00@hotmail.com>
2023-01-25 18:16:01 -0300

    primeiros exemploes de forEach

---
+++ fundamentos/secao-8-HOFs-do-javascript-ES6/dia-1-introducao-a-HOFs/testsAula.js
@@ -0,0 +1,13 @@
+const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+
+arrayOfValues.forEach((element, index) => {
+  console.log(`O elemento atual é: ${element} e possui o index: ${index}`);
+});
+
+const pessoasAprovadas = ['Ana Beatriz', 'Caio Nunes', 'Afonso Ribeiro', 'Leonardo Lins']; // lista de nomes
+
+pessoasAprovadas.forEach((nome, index) => {
+  pessoasAprovadas[index] = nome.toUpperCase(); // acessa cada elemento do array e atualiza para letra maiúscula
+});
+
+console.log(pessoasAprovadas); // ['ANA BEATRIZ', 'CAIO NUNES', 'AFONSO RIBEIRO', 'LEONARDO LINS']
