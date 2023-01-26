20ee75f3aa515de91f5de23f0dc4e7702b9aa640
gabri10on <gabriel.r_00@hotmail.com>
2023-01-26 15:38:07 -0300

    exercicio employees de HOFS

---
+++ fundamentos/secao-8-HOFs-do-javascript-ES6/dia-1-introducao-a-HOFs/employees.js
@@ -0,0 +1,16 @@
+const newEmployees = (employeesGenerator) => {
+  const employees = {
+    id1: employeesGenerator('Pedro Guerra Brasil'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: employeesGenerator('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: employeesGenerator('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+const employeesGenerator = (fullName) => {
+  return {fullName, email: `${fullName.toLowerCase().replaceAll(' ', '_')}@betrybe.com.br`};
+  //ou
+  // return {fullName, email: `${fullName.toLowerCase().split(" ").join("_")}@betrybe.com.br`};
+}
+
+console.log(newEmployees(employeesGenerator))
